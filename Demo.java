d26b3f377acd3dd5bd2057544f78efe7c07eadc1
Sajani <22cis0309@ms.sab.ac.lk>
2026-06-28 10:31:55 +0530

    added the butons using grid layout

--- Demo.java
+++ Demo.java
@@ -1,31 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
-
 class Demo{
 	public static void main(String args[]){
-		JFrame f1=new JFrame("Calculator");	
+		JFrame f1=new JFrame("BorderLayout");	
 		f1.setSize(300,300);
 		f1.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		f1.setLayout(new FlowLayout(FlowLayout.RIGHT));
-		//0->FlowLayout.CENTER
-		//1->FlowLayout.RIGHT
-		//2->FlowLayout.LEFT
-		JButton btAdd=new JButton("Add "); //setText(String)
-		JButton btDelete=new JButton("Delete");
-		JButton btUpdate=new JButton("Update");
-		JButton btView=new JButton("View");
-		btAdd.setFont(new Font("",1,30));
-		btDelete.setFont(new Font("",1,30));
-		btUpdate.setFont(new Font("",1,30));
-		btView.setFont(new Font("",1,30));
-		
-		f1.add(btAdd);
-		f1.add(btUpdate);
-		f1.add(btDelete);
-		f1.add(btView);
-		
+		f1.setLayout(new GridLayout(3,2));
+		JButton[] btArray=new JButton[6];
+		String[] buttonText={"Add","Delete","Update","Read","Cancel","Exit"}; 
+		for (int i = 0; i < 6; i++){
+			btArray[i]=new JButton(buttonText[i]);
+			btArray[i].setFont(new Font("",1,30));
+			f1.add(buttonText[i],btArray[i]);
+		}
 		f1.setVisible(true);
 	}
 }
