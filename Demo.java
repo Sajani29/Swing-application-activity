f3c1fb5efac2fd0a1a61b7722b0ab1ba80982aa0
Sajani <22cis0309@ms.sab.ac.lk>
2026-06-28 09:16:50 +0530

    add small swing gui

---
+++ Demo.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+class Demo{
+	public static void main(String[] args){
+		JFrame f1 = new JFrame("Calculator");
+		f1.setSize(300,300);
+		f1.setVisible(true);
+		f1.setLocationRelativeTo(null);
+	}
+}
